082d6e95f52801b8beb33793ae6338d84fe165d5
zanetowell <zanetowell@gmail.com>
2023-08-07 17:48:24 -0400

    Adds layout to artists route, maps artistsList to page and styles it

--- src/app/artists/page.jsx
+++ src/app/artists/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './page.module.css'
+import { artistsList } from '@/utils/data.js' 
+import Image from 'next/image'
 
 const Artists = () => {
+  const data = artistsList
   return (
-    <div>Artists</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Get to know us!</h1>
+      {data.map(item=>(
+        <div className={styles.artist} key={item.id}>
+          <div className={styles.info}>
+            <h1 className={styles.name}>{item.name}</h1>
+            <p className={styles.bio}>{item.bio}</p>
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img}
+              src={item.pic}
+              fill={true}
+              alt='Artist'
+            />
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -25,3 +25,45 @@ export const navLinks = [
       url: "/contact",
     },
   ];
+
+  export const artistsList = [
+    {
+      id: 1,
+      name: 'Lillian Butler',
+      pic: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=800',
+      bio: 'rubbed station heart train promised shaking ought themselves continent effect nobody composition wheel society still storm engineer perfectly shade while riding similar fun officer',
+      works: [
+        {
+          name: 'program',
+          pic: 'https://images.pexels.com/photos/3246665/pexels-photo-3246665.png?auto=compress&cs=tinysrgb&w=800',
+          desc: 'sign speech remove very carbon box pine distance saw choose opinion actually thus classroom recently powerful silent buy pair question organization harder forget tried',
+        }
+      ],
+    },
+    {
+      id: 2,
+      name: 'Ollie Terry',
+      pic: 'https://images.pexels.com/photos/1121796/pexels-photo-1121796.jpeg?auto=compress&cs=tinysrgb&w=800',
+      bio: 'certain additional grade two trouble have famous course excitement reader offer face brush anywhere explain manufacturing rising material pan this own original explore your',
+      works: [
+        {
+          name: 'diameter',
+          pic: 'https://images.pexels.com/photos/4272616/pexels-photo-4272616.jpeg?auto=compress&cs=tinysrgb&w=800',
+          desc: 'silence occur medicine mind rock orbit me best difference tribe noted would wonderful species short daughter teach aware pencil charge said play nearby useful',
+        }
+      ],
+    },
+    {
+      id: 3,
+      name: 'Sam Hawkins',
+      pic: 'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=800',
+      bio: 'anyway fur officer friendly other printed deeply slowly improve science pure verb cover steady fog vegetable location pine gain noon single stuck center congress',
+      works: [
+        {
+          name: 'welcome',
+          pic: 'https://images.pexels.com/photos/5789583/pexels-photo-5789583.jpeg?auto=compress&cs=tinysrgb&w=800',
+          desc: 'open especially remarkable till free shaking stems front shallow thy though magic leaf everyone yourself becoming mainly sick passage went fat friendly except vertical',
+        }
+      ],
+    },
+  ]
