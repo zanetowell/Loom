54122ae25d2a40d8cb706659a51e2dcab7bf30e5
zanetowell <zanetowell@gmail.com>
2023-08-17 19:59:10 -0400

    Fixes mobile styling for home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,12 @@ import Button from '@/components/Button/Button'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
+      <div className={styles.item1}>
         <h2 className={styles.title}>Welcome to a world of artistic expressions</h2>
         <h2 className={styles.desc}>A hub for creatives of all kinds to share their work with the world</h2>
-        <Button text='Meet our creators' url='/artists'/>
+        <Button text='Meet our creators' url='/artists' className={styles.button}/>
       </div>
-      <div className={styles.item}>
+      <div className={styles.item2}>
         <Image
           src={Loom}
           className={styles.img}
